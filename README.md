b20b527dbe32634047ae81ef730bfc7f48180aed
Oleksandr (alex) Horban <20ohorban@gmail.com>
2023-07-30 08:37:48 -0700

    layout

--- README.md
+++ README.md
@@ -10,10 +10,20 @@ Claremont McKenna College
 As Machine Learning models became more and more sophisticated to adapt to a growing amount of data, humans' ability to understand why these models make decisions that they make decreased. Trying to interpret a decision tree is very different from trying to interpret a deep neural network, that from the outside perspective looks like a **black box**. All we know is that data comes in and magically an answer comes out. Trying to understand what each neuron is doing will get you a one-way ticket to having a mental breakdown. This becomes a problem when interpreting a decision made by a machine learning model becomes a critical step. Consider, for example, working on a problem of binary classification of apples and oranges. Your deep neural network can learn from millions of pictures and be able to differentiate apples from oranges with 99% accuracy. One percent of apples are labeled as oranges, not the end of the world, right? In contrast, consider a risk assessment model that classifies whether the defendant should or should not be sentenced to life in prison. Now, the 1% percent of wrongfully incarcerated inmates carries a much higher price. To top it all off, we can't even tell why our model made the decision to put that 1% of people in jail. It could be the number of crimes they committed in the past, or it could be their race. In such cases understanding why the machine learning model made the decision that it did is very crucial. In reality, such an algorithm exists and is making the decision of putting people in jail. It is called COMPAS and was made by Northpointe. In this paper, I will explain techniques for ML interpretability and run through the example of applying such techniques in the criminal justice system with the hopes of improving algorithms such as COMPAS.
 
 ## Interpretable Models
-There are machine learning models that make interpretability an easy task. Linear regression, logistic regression and the decision tree are commonly used interpretable models.
+There are machine learning models that make interpretability an easy task. Linear regression, logistic regression and the decision tree are commonly used interpretable models. For models like these, it is fairly easy to understand why the model made the decision that it did.
+### Linear Regression
 
 
-## LIME
+
+## Local Model-agnostic Methods
+Local explainability methods explain the behavior of the model at a specific data point (individual prediction). Model-agnostic interpretation methods are ones that work the same no matter what model they are applied to.
+
+### Local Surrogate Models
+
+**Local:** estimates the blackbox around a specific proximity, gives interpretable results for specific input data point.
+**Global:** estimates and explains the entire model.
+**Model-agnostic:** gives interpretable results no matter what family is the blackbox from (even deep neural networks)
+**Surrogate:** a simpler, explainable model.
 
 ## SHAP
 
---
+++ xgboost_explain_predict.py
@@ -0,0 +1,90 @@
+# Importing necessary libraries
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+import shap
+
+# Load the CSV file
+file_path = 'data/compas-scores-raw.csv' # Change this to your file path
+data = pd.read_csv(file_path)
+
+# Preprocessing
+# Separating the features and target
+exclude_columns = ['ScoreText', 'DecileScore', 'RawScore', 'DisplayText', 'RecSupervisionLevelText', 'RecSupervisionLevel', 'Person_ID', 'AssessmentID', 'Case_ID']
+
+X = data.drop(columns=exclude_columns)
+y = data['ScoreText']
+
+# Encoding the target variable
+label_encoder_y = LabelEncoder()
+y_encoded = label_encoder_y.fit_transform(y)
+
+# Copy the original dataset before encoding
+X_original = X.copy()
+
+# Encoding the categorical features and storing the encoders
+encoders = {}
+for col in X.columns:
+    if X[col].dtype == 'object':
+        label_encoder_x = LabelEncoder()
+        X[col] = label_encoder_x.fit_transform(X[col].astype(str))
+        encoders[col] = label_encoder_x
+
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+
+# Train the XGBoost model
+model = xgb.XGBClassifier(objective='multi:softprob', num_class=3)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Model Accuracy: {:.2f}%".format(accuracy * 100))
+
+# Explain predictions using SHAP
+# Create a tree explainer
+explainer = shap.Explainer(model)
+
+# Replace the encoded values with original text in the instance to be explained
+instance_index = 0
+instance_to_explain = X_test.iloc[[instance_index]].copy() # create a copy to avoid warnings
+original_instance = X_original.iloc[[instance_index]].copy()
+
+# Map back the encoded features to the original text using the stored encoders
+for col in instance_to_explain.columns:
+    if col in encoders:
+        instance_to_explain[col] = instance_to_explain[col].apply(lambda x: encoders[col].inverse_transform([x])[0])
+
+# Compute SHAP values for the instance to explain (using encoded data)
+instance_to_explain = X_test.iloc[[instance_index]] # use encoded data
+shap_values = explainer(instance_to_explain)
+class_index = 2 # 2 = High, 1 = Medium, 0 = Low
+instance_shap_values = shap_values.values[0, :, class_index]
+
+# Get the original feature names
+original_instance = X_original.iloc[instance_index]
+
+# Reconstruct the original labels for the features
+original_labels = []
+for col in original_instance.index:
+    if col in encoders:
+        value = encoders[col].inverse_transform([instance_to_explain[col].iloc[0]])[0]
+        original_labels.append(f"{col} = {value}")
+    else:
+        original_labels.append(col)
+
+# Sort SHAP values and corresponding feature names
+sorted_indices = np.argsort(instance_shap_values)
+sorted_shap_values = instance_shap_values[sorted_indices]
+sorted_feature_names = [original_labels[i] for i in sorted_indices]
+
+# Plot the waterfall chart
+plt.barh(sorted_feature_names, sorted_shap_values)
+plt.xlabel('SHAP Value')
+plt.title('Waterfall Plot')
+plt.show()
